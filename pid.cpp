d48f8558a8d0652fe41918cb91d6e71770c1c243
D <89310065+tuduan1902@users.noreply.github.com>
2023-06-10 09:24:48 +0700

    Update pid.cpp

--- pid.cpp
+++ pid.cpp
@@ -82,10 +82,26 @@ void loop() {
   if((nowtime - starttime) >= 10) {
     //Serial.println("abc");
     //Serial.println(delta_time);
-    setpoint1 += convert(0);
-    setpoint2 += convert(0);
-    setpoint1 = -12000;
-    setpoint2 = -12000;  
+    if(Serial.available() > 0) { //manual control of wheesls via terminal
+            char c = Serial.read();
+
+            if(c == 'a') {
+               Setpoint1 += convert(1);
+               Setpoint2 += convert(1);
+            } else if( c == 'b') {
+               Setpoint1 += convert(-1);
+               Setpoint2 += convert(-1);
+            } else if( c == 'l') {
+               Setpoint1 += convert(1);
+               Setpoint2 += convert(0);
+            } else if( c == 'r') {
+               Setpoint1 += convert(0);
+               Setpoint2 += convert(1);
+            } else if (c == 'p') {
+               Setpoint1 += convert(0);
+               Setpoint2 += convert(0);
+            }
+         } 
     input1 = myEnc1.read();
     input2 = myEnc2.read();
     Serial.println(input2);
@@ -96,71 +112,5 @@ void loop() {
     Drive_MotorB(output2);
  
     starttime = millis();
-  }
-}
-
-
-
-// Control 1 engine 
-#include <Encoder.h>
-#include <PID_v1.h>
-#include <Math.h>
-#define ENA 10 // PWM outputs to L298N H-Bridge motor driver module
-const int IN1=11;
-const int IN2=12;
-double r =0.1;
-double kp = 0.2, ki = 0, kd = 0;             // modify for optimal performance
-double input = 0, output = 0, setpoint = 0;
-double starttime;
-double encoder_res = 4800; // enconder resolution 
-Encoder myEnc(2,4);
-
-PID myPID(&input, &output, &setpoint, kp, ki, kd, DIRECT);  // if motor will only run at full speed try 'REVERSE' instead of 'DIRECT'
-
-void setup() {
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
-  digitalWrite(ENA, HIGH);
-  myPID.SetMode(AUTOMATIC);
-  myPID.SetSampleTime(1);
-  myPID.SetOutputLimits(-255, 255);
-  Serial.begin (115200);
-  starttime = 0;
-  input = myEnc.read(); // read value of original position 
-}
-
-void loop() {
-  double nowtime = millis();
-  //Serial.println(nowtime);
-  if((nowtime - starttime) >= 10) {
-    //Serial.println("abc");
-    //Serial.println(delta_time);
-    setpoint += convert(0);
-    input = myEnc.read();
-    Serial.println(input);
-    myPID.Compute();
-    Drive_Motor(output);
-    starttime = millis();
-  }
-}
-
-double convert(double tocdo) {
-  double y = ((60*tocdo))/((2*3.14*r)); // vòng/phút
-  //Serial.println(y);
-  double pulse = (y*encoder_res)/(60*100); // convert to pulse/10ms
-  //Serial.println(pulse);
-  return pulse;
-}
-
-void Drive_Motor(int out) {
-  //Serial.println(out);
-  if (out > 0){
-    digitalWrite(IN1,LOW);
-    digitalWrite(IN2,HIGH);
-    analogWrite(ENA, out);
-  }else {
-    digitalWrite(IN1,HIGH);
-    digitalWrite(IN2,LOW);
-    analogWrite(ENA, abs(out));
   }
 }
